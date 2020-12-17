23f559766118d6b26939daf34e46a43fb6bd427e
Rajit Gohel <59544276+Rajit28@users.noreply.github.com>
2020-12-17 16:19:20 -0500

    Pdf generation for receipt

--- src/components/OrderScreen.js
+++ src/components/OrderScreen.js
@@ -1,13 +1,20 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useContext} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { createOrder, detailsOrder } from '../actions/orderActions';
+import { AuthContext } from '../firebase/Auth';
+import firebase from 'firebase/app'
+import 'firebase/auth';
+import Button from "react-bootstrap/Button";
+import { jsPDF } from "jspdf";
 function OrderScreen(props) {
 
   const orderId = props.match.params.id;
   const orderDetails = useSelector((state) => state.orderDetails);
   const { order, loading, error } = orderDetails;
   const orderPay = useSelector((state) => state.orderPay);
+  const { currentUser } = useContext(AuthContext);
+
 
   const dispatch = useDispatch();
   useEffect(() => {
@@ -16,8 +23,60 @@ function OrderScreen(props) {
   }, [orderId]);
 
   return loading ? <div>Loading ...</div> : error ? <div>{error}</div> :
-
     <div>
+      <div>
+        <Button
+          variant="primary"
+          style={{ marginBottom: "15px" }}
+          onClick={(props) => {
+
+            const doc = new jsPDF();
+            console.log('reached carItems');
+            let orderstr = "";
+            let tcost = 0;
+            order.orderItems.map((i) => {
+              orderstr += "\n" +
+                "Item Name: " + i.name + "\n" +
+                "Price: " + i.price + "\n" +
+                "Quantity: " + i.qty + "\n";
+
+              tcost += i.price;
+
+
+            })
+
+            console.log("tcost is " + tcost);
+            console.log(currentUser.metadata.lastSignInTime);
+
+
+
+
+            // console.log(cartItems)
+            // let a = {
+            //   name: cartItems[1].name,
+            //   price: cartItems[1].price,
+            //   qty: cartItems[1].qty
+            // };
+            // console.log(JSON.stringify(a))
+            // doc.text(`${currentUser}`);
+            // doc.text(`${JSON.stringify(a.name)}, Thank you for Shopping with us! This is the receipt for your shopping`, 10, 10);
+            // doc.setFontSize(10)
+            doc.setFontSize(30).text(`Order Receipt for ${currentUser.email}`, 10, 10)
+            doc.setFontSize(20).text(`The shipping cost is ${order.shippingPrice}.`, 10, 30)
+            doc.setFontSize(20).text(`The total tax is ${order.taxPrice}.`, 10, 40)
+            doc.setFontSize(20).text(`The total cost is : ${order.totalPrice}`, 10, 50)
+            doc.setFontSize(10).text(`Order placed on ${currentUser.metadata.lastSignInTime}. Thank you for Shopping with us!`, 10, 60);
+            doc.setFontSize(10).text("Contact us at customercare@business.com for enquiry", 10, 70);
+            doc.setFontSize(10).text(`It will be shipped to ${order.shipping.address} ${order.shipping.city} ${order.shipping.postalCode}${order.shipping.country}`, 10, 80)
+            doc.setFontSize(10).text(`The payment was done by ${order.payment.paymentMethod}`, 10, 90)
+            doc.setFontSize(10).text(orderstr, 10, 100);
+            doc.save("Order_receipt.pdf");
+          }}
+        >
+          Download Receipt PDF
+        </Button>
+      </div>
+
       <div className="placeorder">
         <div className="placeorder-info">
           <div>
